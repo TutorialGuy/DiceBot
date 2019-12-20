ca67634934b87625986ff0864374966f294afbc6
TutorialGuy <uakuni@Gmail.com>
2019-12-20 03:23:19 +0200

    add feature with dice counts

--- bot_origin.py
+++ bot_origin.py
@@ -1,25 +1,41 @@
 from discord import client
 import discord
 import random
+import re
 
 
 tokendice = ""
 client = discord.Client()
 
 
-def dice_throw(dicetype):
-    dicedrop = random.randrange(1,int(dicetype))
-    return f'{dicedrop}'
+def dice_throw(dicecount, dicetype):
+    dice_res = []
+    for i in range(dicecount):
+        dicedrop = random.randrange(1, int(dicetype)+1)
+        dice_res.append(dicedrop)
+    return f'{dice_res}'
+
+
+def parse_command(command):
+    matches = re.findall(r'\d+', command)
+    return matches
 
 
 @client.event
 async def on_message(message):
     print(f'{message.author} send message {message.content}')
-    if f"{message.content}".startswith("!d"):
-        dicetype = f"{message.content}".split(sep="d")[1]
-        result = dice_throw(dicetype)
-        response = f"{result}"
-        await message.channel.send(response)
+    if f"{message.content}".startswith(f"throw"):
+        try:
+            dices = f'{message.content}'.split(" ")[1]
+        except IndexError:
+            return
+        if f"{message.content}".startswith(f"throw {dices}"):
+            patern = parse_command(message.content)
+            dicecount = int(patern[0])
+            dicetype = patern[1]
+            result = dice_throw(dicecount,dicetype)
+            response = f"{result}"
+            await message.channel.send(response)
 
 
 @client.event
